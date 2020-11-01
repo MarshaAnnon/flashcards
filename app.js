4852b8d14d34b3ab4246fbdd0f9752dd0fbcb272
marsha <marshaannon@gmail.com>
2020-10-31 20:10:59 -0400

    add pug, views, & index.pug

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, resp) => {
-    resp.send('I am proud to work for MediaMath!')
-})
+app.set("view engine", "pug");
 
-app.listen(3000);
+app.get("/", (req, res) => {
+    res.render("index")
+});
+
+app.get("/hello", (req, res) => {
+    res.send("<h1>Hello, I'm a new developer at MediaMath!</h1>")
+});
+
+app.listen(3000, () => {
+    console.log("The application is running on localhost:3000")
+});
