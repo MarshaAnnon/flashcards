6313760dd671ca9f49ac5b246a9516865142c2b5
marsha <marshaannon@gmail.com>
2020-11-11 21:14:25 -0500

    remove routes, require mainRoutes & cardRoutes & app.use middleware for the routes

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 app.set("view engine", "pug");
+
+const mainRoutes = require("./routes");
+const cardRoutes = require("./routes/cards");
+
+app.use(mainRoutes);
+app.use("/cards", cardRoutes);
 
 app.use((req, res, next) => {
     const err = new Error("Internal Server Error")
@@ -20,37 +25,6 @@ app.use((req, res, next) => {
     console.log("hello");
     next();
 })
-app.get("/", (req, res) => {
-    const name = req.cookies.username
-    if (name) {
-        res.render("index", { name })
-    } else {
-        res.redirect("/hello")
-    }
-});
-
-app.get("/cards", (req, res) => {
-    res.render("card", { prompt: "Who is buried in Grant's tomb?" });
-});
-
-app.get("/hello", (req, res) => {
-    const name = req.cookies.username
-    if (name) {
-        res.redirect("/")
-    } else {
-        res.render("hello")
-    }
-});
-
-app.post("/hello", (req, res) => {
-    res.cookie("username", req.body.username)
-    res.redirect("/");
-});
-
-app.post("/goodbye", (req, res) => {
-    res.clearCookie("username")
-    res.redirect("/hello");
-});
 
 app.use((req, res, next) => {
     const err = new Error("Not Found");
