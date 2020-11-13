1264a5faa9815c2442277aa6b871f06f60af2d07
marsha <marshaannon@gmail.com>
2020-11-12 20:08:44 -0500

    add link to next card

--- routes/cards.js
+++ routes/cards.js
@@ -3,13 +3,25 @@ const router = express.Router();
 const { data } = require("../data/flashcardData.json");
 const { cards } = data;
 
+router.get("/", (req, res) => {
+    const numberOfCards = cards.length;
+    const flashCardId = Math.floor(Math.random() * numberOfCards);
+    res.redirect(`/cards/${flashCardId}?side=question`)
+});
+
 router.get("/:id", (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
+
+    if (!side) {
+        res.redirect(`/cards/${id}`)
+    }
+
+    const name = req.cookies.username;
     const text = cards[id][side];
     const { hint } = cards[id];
     
-    const templateData = { id, text };
+    const templateData = { id, text, name };
     
     if (side === "question") {
         templateData.hint = hint;
