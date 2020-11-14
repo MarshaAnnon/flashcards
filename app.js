c5060dc43515b33c95b5dd2afaa0afff03a437dd
marsha <marshaannon@gmail.com>
2020-11-14 12:38:08 -0500

    remove repetitive code

--- app.js
+++ app.js
@@ -18,25 +18,14 @@ app.use("/cards", cardRoutes);
 
 app.use((req, res, next) => {
     const err = new Error("Internal Server Error")
-    err.status = 500;
-    next(err);
-});
-
-app.use((req, res, next) => {
-    console.log("hello");
-    next();
-})
-
-app.use((req, res, next) => {
-    const err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render("error");
+res.locals.error = err;
+res.status(err.status);
+res.render("error");
 });
 
 app.listen(3000, () => {
